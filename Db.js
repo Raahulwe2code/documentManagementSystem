3b60b58b4df346f27871ab3d8a6870f8add658f5
Raahulwe2code <rahul.verma.we2code@gmail.com>
2023-07-03 10:43:40 +0530

    after console clver

--- Db.js
+++ Db.js
@@ -10,20 +10,32 @@ import "dotenv/config";
 // A package.json file containing the fields "name", "version" and "script.start" (or "main") is required.
 // The application must listen on port 8080.
 // Documentation about Node.Js is available!
+
+// db_name= documentdb
+// Server:- daigle.iad1-mysql-e2-4b.dreamhost.com
+// Username: indiakinursery
+// pwd: WE2code@2023
 console.log();
+const connection = mysql.createConnection({
+  host: "bwoill6ixggdovjdxfzw-mysql.services.clever-cloud.com",
+  user: "ubkxmigstvkzomce",
+  password: "38i6RSJ023zN3RGfbC36",
+  database: "bwoill6ixggdovjdxfzw",
+});
+
 // const connection = mysql.createConnection({
-//   host: "bwoill6ixggdovjdxfzw-mysql.services.clever-cloud.com",
-//   user: "ubkxmigstvkzomce",
-//   password: "38i6RSJ023zN3RGfbC36",
-//   database: "bwoill6ixggdovjdxfzw",
+//   host: "localhost",
+//   user: "we2code",
+//   password: "we2code",
+//   database: "DocumentDb",
 // });
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "we2code",
-  password: "we2code",
-  database: "DocumentDb",
-});
+// const connection = mysql.createConnection({
+//   host: "daigle.iad1-mysql-e2-4b.dreamhost.com",
+//   user: "indiakinursery",
+//   password: "WE2code@2023",
+//   database: "documentdb",
+// });
 connection.connect((error) => {
   if (error) throw error;
   console.log("Successfully connected to the database.");
